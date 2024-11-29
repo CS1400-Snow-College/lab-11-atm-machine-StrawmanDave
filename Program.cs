1847fd5f5b60dff2893e84a71e7b089bcf5e8dbe
StrawmanDave <davidgb.strawman@gmail.com>
2024-11-29 08:13:15 -0700

    Improved machine and no more negative accounts

--- Program.cs
+++ Program.cs
@@ -4,35 +4,47 @@ string[] DataBase = File.ReadAllLines("bank.txt");
 
 Console.WriteLine("Please enter your username.");
 string getUsername = Console.ReadLine();
+string name = "";
 
+string SaveData = "";
+string subData = "";
+
+string welcomeMenu = @$"
+Welcome {name} what would you like to do?
+1)Check Balance
+2)Withdraw
+3)Deposit
+4)Display last transactions
+5)Quick Withdraw $40
+6)Quick Widthraw $100
+7)End current session
+Please pick the number corresponding to the option.
+";
 
 for(int i = 0; i<DataBase.Length; i++)
 {
     string[] splitLines = DataBase[i].Split(",");
-    List<string> Split = splitLines.ToList();
-    for(int j = 0; j<Split.Count; j++)
+    for(int j = 0; j<splitLines.Length; j++)
     {
-        if(getUsername == Split[j])
+        if(getUsername == splitLines[j])
         {
             Console.WriteLine("What is your Pin?");
             string getPin = Console.ReadLine();
-            if(getPin == Split[j+1])
+            if(getPin == splitLines[j+1])
             {
                 Console.Clear();
-                string welcomeMenu = @$"
-                Welcome {Split[j]} what would you like to do?
-
-                1)Check Balance
-                2)Withdraw
-                3)Deposit
-                4)Display last transactions
-                5)Quick Withdraw $40
-                6)Quick Widthraw $100
-                7)End current session
-
-                Please pick the number correspoinding to the option.
-                ";
-
+                name = splitLines[j];
+                welcomeMenu = @$"
+Welcome {name} what would you like to do?
+1)Check Balance
+2)Withdraw
+3)Deposit
+4)Display last transactions
+5)Quick Withdraw $40
+6)Quick Widthraw $100
+7)End current session
+Please pick the number corresponding to the option.
+";
                 Console.WriteLine(welcomeMenu);
                 Queue<string> lastTransactions = new Queue<string>();
                 bool exit = false;
@@ -49,25 +61,27 @@ for(int i = 0; i<DataBase.Length; i++)
                         switch(choice)
                         {
                             case 1:
-                                Console.WriteLine($"Your current balance stands at {Split[j + 2]}");
+                                Console.WriteLine($"Your current balance stands at {splitLines[j + 2]}");
                                 break;
                             case 2:
                                 Console.WriteLine("How much would you like to withdraw?");
-                                string dollarSign = Split[j+2].Substring(0,1);
-                                string stringBalance = Split[j+2].Substring(1);
+                                string dollarSign = splitLines[j+2].Substring(0,1);
+                                string stringBalance = splitLines[j+2].Substring(1);
                                 decimal getCurrentBalance = ReadDecimal(stringBalance);
                                 //Console.WriteLine(Withdraw(ReadDecimal(Console.ReadLine()), getCurrentBalance));
-                                Split[j+2] = dollarSign + decimalToString(Withdraw(ReadDecimal(Console.ReadLine()), getCurrentBalance));
-                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {Split[j+2]}");
+                                splitLines[j+2] = dollarSign + decimalToString(Withdraw(ReadDecimal(Console.ReadLine()), getCurrentBalance));
+                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {splitLines[j+2]}");
+                                Console.WriteLine("Account updated what next?");
                                 break;
                             case 3:
                                 Console.WriteLine("How much would you like to deposit?");
-                                stringBalance = Split[j+2].Substring(1);
-                                dollarSign = Split[j+2].Substring(0,1);
+                                stringBalance = splitLines[j+2].Substring(1);
+                                dollarSign = splitLines[j+2].Substring(0,1);
                                 getCurrentBalance = ReadDecimal(stringBalance);
                                 //Console.WriteLine(Deposit(ReadDecimal(Console.ReadLine()), getCurrentBalance));
-                                Split[j+2] = decimalToString(Deposit(ReadDecimal(Console.ReadLine()), getCurrentBalance));
-                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {Split[j+2]}");
+                                splitLines[j+2] = dollarSign + decimalToString(Deposit(ReadDecimal(Console.ReadLine()), getCurrentBalance));
+                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {splitLines[j+2]}");
+                                Console.WriteLine("Account updated what next?");
                                 break;
                             case 4:
                                 foreach(string transaction in lastTransactions)
@@ -76,20 +90,22 @@ for(int i = 0; i<DataBase.Length; i++)
                                 }
                                 break;
                             case 5:
-                                stringBalance = Split[j+2].Substring(1);
-                                dollarSign = Split[j+2].Substring(0,1);
+                                stringBalance = splitLines[j+2].Substring(1);
+                                dollarSign = splitLines[j+2].Substring(0,1);
                                 getCurrentBalance = ReadDecimal(stringBalance);
                                 //Console.WriteLine(Withdraw(40,getCurrentBalance));
-                                Split[j+2] = dollarSign + decimalToString(Withdraw(40, getCurrentBalance));
-                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {Split[j+2]}");
+                                splitLines[j+2] = dollarSign + decimalToString(Withdraw(40, getCurrentBalance));
+                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {splitLines[j+2]}");
+                                Console.WriteLine("Account updated what next?");
                                 break;
                             case 6:
-                                stringBalance = Split[j+2].Substring(1);
-                                dollarSign = Split[j+2].Substring(0,1);
+                                stringBalance = splitLines[j+2].Substring(1);
+                                dollarSign = splitLines[j+2].Substring(0,1);
                                 getCurrentBalance = ReadDecimal(stringBalance);
                                 //Console.WriteLine(Withdraw(100,getCurrentBalance));
-                                Split[j+2] = dollarSign + decimalToString(Withdraw(100, getCurrentBalance));
-                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {Split[j+2]}");
+                                splitLines[j+2] = dollarSign + decimalToString(Withdraw(100, getCurrentBalance));
+                                lastTransactions.Enqueue($"{dollarSign}{stringBalance} to {splitLines[j+2]}");
+                                Console.WriteLine("Account updated what next?");
                                 break;
                             case 7:
                                 exit = true;
@@ -100,7 +116,8 @@ for(int i = 0; i<DataBase.Length; i++)
                         }
                     }catch(FormatException)
                     {
-                        Console.WriteLine("Sorry that was not an option");
+                        Console.WriteLine("Sorry that was not an option or your input did not register");
+                        Console.WriteLine(welcomeMenu);
                     }
                 }while(exit == false);
             }else
@@ -108,20 +125,60 @@ for(int i = 0; i<DataBase.Length; i++)
                 Console.WriteLine("Incorrect pin please try again");
             }
         }
+        subData = subData + "," + splitLines[j];
     }
+    subData = subData.Substring(1);
+    SaveData = SaveData + subData + '\n';
+    subData = "";
 }
+
+
+//Console.WriteLine();
+//Console.WriteLine(SaveData);
+File.WriteAllText("bank.txt", SaveData);
 
 
 decimal Withdraw (decimal amountWithdraw, decimal currentBalance)
 {
-    decimal newBalance = currentBalance - amountWithdraw;
-    return newBalance;
+    try
+    {
+        if(currentBalance < amountWithdraw)
+        {
+            throw new ArgumentException("You do not have enough funds to do that.");
+        }
+
+        if(amountWithdraw < 0)
+        {
+            throw new FormatException();
+        }
+        decimal newBalance = currentBalance - amountWithdraw;
+        return newBalance;
+    }catch(ArgumentException e)
+    {
+
+        Console.WriteLine(e.Message);
+        Console.WriteLine(welcomeMenu);
+        return currentBalance;
+    }
 }
 
-decimal Deposit (decimal amountDeposit, decimal currentBalance)
+decimal Deposit(decimal amountDeposit, decimal currentBalance)
 {
-    decimal newBalance = currentBalance + amountDeposit;
-    return newBalance;
+    try
+    {
+        if(amountDeposit < 0)
+        {
+            throw new ArgumentException("That is a withdraw not a depoist");
+        }
+
+        decimal newBalance = currentBalance + amountDeposit;
+        return newBalance;
+    }catch(ArgumentException e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(welcomeMenu);
+        return currentBalance;
+    }
 }
 
 decimal ReadDecimal(string getInput)
@@ -129,15 +186,7 @@ decimal ReadDecimal(string getInput)
     decimal number = -1;
     do
     {
-        try
-        {
-            number = Convert.ToDecimal(getInput);
-        }
-        catch(ArgumentOutOfRangeException)
-        {
-            number = -1;
-            Console.WriteLine("That does not register");
-        }
+        number = Convert.ToDecimal(getInput);
     }while(number == -1);
     return number;
 }
--- obj/Debug/net8.0/lab-11-atm-machine-StrawmanDave.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-11-atm-machine-StrawmanDave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-11-atm-machine-StrawmanDave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aefca5ad8143ed279112c8d1c04d625c26fffff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd281426dd6e11997a0dd96f235e082226cfc052")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-11-atm-machine-StrawmanDave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-11-atm-machine-StrawmanDave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
